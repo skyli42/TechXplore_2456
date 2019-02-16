bc318bb23f13e9e00a2852fe2139b72427bc1a48
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-02-16 16:07:50 -0500

    Full website functionality

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from flask import request
 import flask
+from flask import request
+from flask_socketio import SocketIO
+from flask_socketio import emit
+import random
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route("/")
 def hello():
-    response = "This is a message from the server"
-    return flask.render_template('index.html', response=response)
+    return flask.render_template('index.html')
+
+
+@socketio.on('new sequence')
+def handle_sequence(data):
+    seq = data
+    print(seq)
+    emit('sequence return string', "Sequence Received")
 
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
