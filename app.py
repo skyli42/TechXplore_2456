ba8c91a3fd345ef48cac6cf1d4013247e0aa5b74
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-02-16 15:13:39 -0500

    Server to website functionality

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request
 import flask
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return flask.render_template('index.html')
+    response = "This is a message from the server"
+    return flask.render_template('index.html', response=response)
 
 
 if __name__ == "__main__":
