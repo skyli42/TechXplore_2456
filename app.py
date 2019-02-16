a8ea2e0e92ab3567879b0fa6fd8293f516a14f03
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-02-16 14:14:25 -0500

    Updated paths

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import flask
 app = Flask(__name__)
- 
+
+
 @app.route("/")
 def hello():
-    return "Welcome to Prion Prediction!"
+    return flask.render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
